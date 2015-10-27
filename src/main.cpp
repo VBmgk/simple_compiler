2341fc8942684d2db30a1ab6c319bd0febbb5e07
VBmgk <victorbramigk@gmail.com>
2015-10-27 00:19:48 -0200

    Improved 'test' of lexical analysis

--- src/main.cpp
+++ src/main.cpp
@@ -5,37 +5,21 @@
 #include "syntactical_an.hpp"
 
 int main(void) {
-  //AlexicalAnalizer lex_an("../test_code.lang");
-  //t_token buff;
+  AlexicalAnalizer lex_an("../test_codes/test_code_3.lang");
+  int token_2 = -1;
+  t_token buff = VAR;
 
   printf(" Compilador\n");
-  /*
+  
   printf(" Análise léxica\n");
-  buff = lex_an.nextToken();
-  lex_an.print(buff);
-  buff = lex_an.nextToken();
-  lex_an.print(buff);
-  buff = lex_an.nextToken();
-  lex_an.print(buff);
-  buff = lex_an.nextToken();
-  lex_an.print(buff);
-  buff = lex_an.nextToken();
-  lex_an.print(buff);
-  buff = lex_an.nextToken();
-  lex_an.print(buff);
-  buff = lex_an.nextToken();
-  lex_an.print(buff);
-  buff = lex_an.nextToken();
-  lex_an.print(buff);
-  buff = lex_an.nextToken();
-  lex_an.print(buff);
-  buff = lex_an.nextToken();
-  lex_an.print(buff);
-  buff = lex_an.nextToken();
-  lex_an.print(buff);
-  buff = lex_an.nextToken();
-  lex_an.print(buff);
-  */
+  for (int i=0; buff != END_OF_CODE ;i++) {
+    buff = lex_an.nextToken();
+    token_2 = lex_an.getLastToken2();
+    lex_an.print(buff);
+    if (token_2 >= 0) lex_an.print_const(token_2);
+  }
+
+  /*
   SyntacticalAn syn_an("../test_codes/test_code.lang","../compilada.csv");
   switch(syn_an.check_syntax()) {
     case EXIT_LEXICAL_ERROR:
@@ -48,6 +32,7 @@ int main(void) {
       printf("syntax correct!!!\n");
       break;
   }
+  */
 
   return EXIT_SUCCESS;
 }
